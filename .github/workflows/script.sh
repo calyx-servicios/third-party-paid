5dabdca8a025f09a929edf583d8923fe1da89a05
Fede Gregori <jfedericogregori@gmail.com>
2022-03-10 03:01:23 -0300

    [Add] Auto deploy on odoo13-test changed to rsync.

--- .github/workflows/script.sh
+++
@@ -1,5 +0,0 @@
-cd /opt/odoo13/custom-addons/calyx/third-party-paid
-eval `ssh-agent -s`
-ssh-add ~/.ssh/third-party-paid
-git fetch --all; git reset --hard HEAD; git merge @{u}
-service odoo13 restart
