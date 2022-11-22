6c5afa5321d5383b5680ff31908ae951d6ce549a
cristian <crisdparadiso@gmail.com>
2022-11-22 11:29:33 -0300

    [ODOO-5360][ADD][odoo_connector_api]

--- search_assistant/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-# Test Case For Bloopark by Agustin Wisky <agustinwisky@gmail.com>
-
-from . import models
-from . import wizard
