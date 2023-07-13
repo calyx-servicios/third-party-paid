b3c8fc6f9e41055a673a3f7d3b25f0e54caac2e9
gabbiiperez <gabbiiperez94@gmail.com>
2023-07-13 14:41:47 -0300

    [ODOO-6916][IMP][melisync]

--- melisync/__manifest__.py
+++ melisync/__manifest__.py
@@ -2,7 +2,7 @@
 {
     'name': "MercadoLibre Uploader",
     'summary': "MercadoLibre Uploader for Odoo.",
-    'version': '1.0.2',
+    'version': '1.0.3',
     'author': "Lucas Shoobridge",
     'website': "https://www.linkedin.com/in/lshoobridge/",
     'description': """
