c64bdebc69bed113a2d889f8b6172c793b5b1a3c
enzogonzalezdev <enzogonzal@gmail.com>
2024-03-18 18:43:35 -0300

    [ODOO-6957][FIX][globalteckz_magento_2]

--- globalteckz_magento_2/models/payment_magento.py
+++ globalteckz_magento_2/models/payment_magento.py
@@ -22,7 +22,9 @@
 from odoo import fields,models,api
 import requests
 import json
-
+from odoo.exceptions import UserError
+import logging
+logger = logging.getLogger('product')
 
 class PaymentMethodMagento(models.Model):
     _name = "payment.method.magento"
@@ -31,18 +33,27 @@ class PaymentMethodMagento(models.Model):
     name =  fields.Char('Name')
     code =  fields.Char('code')
     
-    
 
-    def create_payment_method(self,shop_data,headers,payment_id):
-        url=str(shop_data.magento_instance_id.location)+"/rest/V1/carts/"+str(payment_id)+"/payment-methods"
-        response = requests.request("GET",url, headers=headers) 
-        if str(response.status_code)=="200":
-            payment_code=json.loads(response.text)
-            if payment_code:
-                for payment in payment_code:
-                    mag_id = self.search([('code','=',payment.get('code'))])
-                    if not mag_id:
-                        self.create({'name':payment['title'], 'code':payment['code']})
-        
-            self._cr.commit()
-        return True
+    def create_payment_method(self, shop_data, headers, order_id):
+        url = f"{shop_data.magento_instance_id.location}/rest/V1/orders/{order_id}"
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            order_data = response.json()
+            payment_data = order_data.get('payment', {})
+            payment_method_code = payment_data.get('method')
+            payment_method_title = payment_data['additional_information'][0]         
+            if payment_method_code:
+                mag_id = self.search([('code', '=', payment_method_code)])
+                if not mag_id:
+                    met_id = self.create({'name': payment_method_title, 'code': payment_method_code})
+                    return met_id
+                else:
+                    if mag_id['name'] != payment_method_title:
+                        mag_id.write({'name': payment_method_title})
+                self._cr.commit()
+                return True
+            else:
+                return False
+        else:
+            logger.error(f"Error fetching order details. Status Code: {response.status_code}")
+            return False
