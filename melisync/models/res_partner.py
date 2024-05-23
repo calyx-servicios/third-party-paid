11d6cd444753ea992b20112ad5f8535f9565a45a
enzogonzalezdev <enzogonzal@gmail.com>
2024-05-23 02:12:29 -0300

    [ODOO-7737][FIX][MELISYNC]

--- melisync/models/res_partner.py
+++ melisync/models/res_partner.py
@@ -5,4 +5,4 @@ class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     meli_id = fields.Char(unique=True, string=_('Meli ID'))
-    meli_shipping_id = fields.Char(unique=True, string=_('Mercado Libre Shipping ID:')) 
+    meli_shipping_id = fields.Char(unique=True, string=_('Mercado Libre Shipping ID:'))
